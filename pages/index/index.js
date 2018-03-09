6c358ed57e85fcb9b807e741493fa5efc533e542
GuoZhiHao <1044654709@qq.com>
2018-03-09 16:58:22 +0800

    修改定位数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,100 +1,105 @@
 // pages/index/index.js
+var mapCtx = null;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    longitude:0,
-    latitude:0
+    longitude: 0,
+    latitude: 0,
   },
-  bindcontroltap: function(e) {
-    switch(e.controlId) {
+  onReady: function (res) {
+    mapCtx = wx.createMapContext("map");
+    console.log("初始位置", mapCtx);
+  },
+  bindcontroltap: function (e) {
+    switch (e.controlId) {
       case 1:
         this.movetoCenter();
         break;
       case 2:
-         if(this.timer){
-            wx.navigateBack({
-               delta: 1
-            })
-         }else{
-            wx.scanCode({
-               success: () => {
-                  wx.showLoading({
-                     title: '正在获取密码',
-                  })
-                  wx.request({
-                     url: 'https://www.easy-mock.com/mock/5963172d9adc231f357c8ab1/ofo/getname',
+        if (this.timer) {
+          wx.navigateBack({
+            delta: 1
+          })
+        } else {
+          wx.scanCode({
+            success: () => {
+              wx.showLoading({
+                title: '正在获取密码',
+              })
+              wx.request({
+                url: 'https://www.easy-mock.com/mock/5963172d9adc231f357c8ab1/ofo/getname',
 
-                     success: (res) => {
-                        console.log(res);
-                        wx.hideLoading();
-                        wx.redirectTo({
-                           url: '../scanResult/index?password=' + res.data.data.password + '&number=' + res.data.data.number,
-                           success: () => {
-                              wx.showToast({
-                                 title: '获取密码成功',
-                                 duration: 1000
-                              })
-                           }
-                        })
-                     }
+                success: (res) => {
+                  console.log(res);
+                  wx.hideLoading();
+                  wx.redirectTo({
+                    url: '../scanResult/index?password=' + res.data.data.password + '&number=' + res.data.data.number,
+                    success: () => {
+                      wx.showToast({
+                        title: '获取密码成功',
+                        duration: 1000
+                      })
+                    }
                   })
-               }
-            })
-         }
-         break;
+                }
+              })
+            }
+          })
+        }
+        break;
       case 3:
-         wx.navigateTo({
-            url: '../my/index',
-         })
-         break;
+        wx.navigateTo({
+          url: '../my/index',
+        })
+        break;
       case 4:
-         wx.navigateTo({
-            url: '../warn/index',
-         })
-         break;
-         
-     }
-   },
+        wx.navigateTo({
+          url: '../warn/index',
+        })
+        break;
+
+    }
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     this.timer = options.timer;
+    this.timer = options.timer;
     wx.getLocation({
-      success: (res)=> {
+      success: (res) => {
         this.setData({
-          longitude : res.longitude,
-          latitude : res.latitude
+          longitude: res.longitude,
+          latitude: res.latitude
         })
       },
     })
-    wx.getSystemInfo({  
-      success: (res)=> {
+    wx.getSystemInfo({
+      success: (res) => {
         this.setData({
           controls: [{
-            id : 1,
-            iconPath : "/images/location.png",
-            position : {
-              width : 40,
+            id: 1,
+            iconPath: "/images/location.png",
+            position: {
+              width: 40,
               height: 40,
-              left : 20,
-              top : res.windowHeight - 80
+              left: 20,
+              top: res.windowHeight - 80
             },
-            clickable : true
-          },{
-            id : 2,
-            iconPath : "/images/use.png",
-            position : {
-              width : 100,
-              left : res.windowWidth/2 - 50,
-              top : res.windowHeight - 120
+            clickable: true
+          }, {
+            id: 2,
+            iconPath: "/images/use.png",
+            position: {
+              width: 100,
+              left: res.windowWidth / 2 - 50,
+              top: res.windowHeight - 120
             },
-            clickable : true
-          },{
+            clickable: true
+          }, {
             id: 3,
             iconPath: "/images/avatar.png",
             position: {
@@ -104,7 +109,7 @@ Page({
               top: res.windowHeight - 80
             },
             clickable: true
-          },{
+          }, {
             id: 4,
             iconPath: "/images/warn.png",
             position: {
@@ -114,7 +119,7 @@ Page({
               top: res.windowHeight - 175
             },
             clickable: true
-          },{
+          }, {
             id: 5,
             iconPath: "/images/marker.png",
             position: {
@@ -125,7 +130,7 @@ Page({
           }]
         })
       },
-    }) 
+    })
   },
   movetoCenter: function () {
     this.mapctx.moveToLocation();
@@ -138,4 +143,40 @@ Page({
     this.mapctx = wx.createMapContext("map");
     this.movetoCenter();
   },
+  bindtap: function (res) {
+    console.log("点击了地图", res);
+  },
+  bindupdated: function (res) {
+    console.log("地图渲染完成", res);
+  },
+  /**
+   * 视野发生变化时触发
+   */
+  bindregionchange: function (res) {
+    // 动作结束
+    if(res.type=="end"){
+      // 获取屏幕中心的经纬度
+      mapCtx.getCenterLocation({
+        success: function (res) {
+          console.log("中心点经纬度", res);
+          var latitude = res.latitude;
+          var longitude = res.longitude;
+          // 将经纬度转换成具体位置
+          wx.request({
+            url: 'http://apis.map.qq.com/ws/geocoder/v1/?location=' + latitude + ',' + longitude + '&key=腾讯地图key',
+            success: (res) => {
+              console.log("具体位置", res);
+              wx.showModal({
+                title: '当前位置',
+                content: res.data.result.address,
+                showCancel: false
+              })
+            }
+          })
+
+        }
+      })
+    }
+   
+  }
 })
